957bee40fcc0f344f6653ddab27c487216acc095
124892735-hash <124892735@qq.com>
2026-04-13 19:15:31 +0800

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -9,12 +9,7 @@ export const profileData = {
   title: "专业剪辑师",
   heroTitleLine1: "Visual",
   heroTitleLine2: "Narrative.",
-  description: "
-经验丰富：涉及多种影片类型：企宣片、广告片、采访片、地产片、各类展厅影片等。
-抗压力强：一直在乙方摩擦历练，练就了一身抗压力。
-认真负责：一直以来同事及领导乃至客户对我的一致评价，也是我觉得在工作中应该有的态度。
-包容性强：非常愿意接受新鲜事物，愿意听取有建设性的意见。
-服务客户：各政府部门、顺丰、腾讯、中兴、深大、招商银行、华侨城集团、华润、中交、中建等。",
+  description: "创意视觉叙事。主题故事片、TVC、企业宣传片等多元领域，用镜头捕捉真实，用光影传递情感，精准传递核心价值观",
   phone: "185-0301-5519",
   email: "124892735@qq.com"
 };
@@ -30,26 +25,110 @@ export const profileData = {
 export const worksData: Work[] = [
   {
     id: '1',
-    title: '我的第一支TVC广告',
-    category: 'TVC广告',
+    title: '共和国没有开闸',
+    category: '故事片',
     // 如果你不想自己找封面，直接把 coverUrl 删掉或者留空，网页会自动显示B站的播放器！
     coverUrl: '', 
-    videoUrl: 'https://www.bilibili.com/video/BV1GJ411x7h7', // 支持B站、YouTube或MP4直链
-    description: '这是我的第一个示例作品，因为没有填封面，所以直接显示了B站播放器。',
+    videoUrl: 'https://www.xinpianchang.com/a12569641?searchKw=%E5%85%B1%E5%92%8C%E5%9B%BD%E6%B2%A1%E6%9C%89%E5%BC%80%E9%97%B8&from=search_post', // 支持B站、YouTube或MP4直链
+    description: '主要负责剪辑和声音设计,参与前期脚本创意和部分镜头创意',
     resolution: '4K',
-    duration: '01:30',
+    duration: '11:22',
     ownerId: 'admin',
     createdAt: new Date().toISOString()
   },
   {
     id: '2',
-    title: '企业宣传片示例',
+    title: '广西桂物宣传片',
     category: '企业宣传片',
-    coverUrl: 'https://images.unsplash.com/photo-1536240478700-b869070f9279?q=80&w=2600&auto=format&fit=crop',
-    videoUrl: 'https://www.bilibili.com/video/BV1xx411c7mD',
-    description: '为某科技公司制作的年度宣传片。',
-    resolution: '1080P',
-    duration: '03:45',
+    coverUrl: '',
+    videoUrl: 'https://www.bilibili.com/video/BV17vnhzpEEi/',
+    description: '主要负责剪辑和声音设计',
+    resolution: '4k',
+    duration: '06:02',
+    ownerId: 'admin',
+    createdAt: new Date(Date.now() - 86400000).toISOString() // 昨天的日期
+  },
+  {
+    id: '3',
+    title: '平潭大练岛宣传片',
+    category: '企业宣传片',
+    coverUrl: '',
+    videoUrl: 'https://www.bilibili.com/video/BV18onGzsEiU/',
+    description: '负责整体项目进度和项目质量,对接客户需求与制作团队,并且负责影片剪辑和声音设计',
+    resolution: '4k',
+    duration: '03:38',
+    ownerId: 'admin',
+    createdAt: new Date(Date.now() - 86400000).toISOString() // 昨天的日期
+  },
+  {
+    id: '4',
+    title: '天津比亚迪tvc',
+    category: 'TVC广告',
+    coverUrl: '',
+    videoUrl: 'https://www.bilibili.com/video/BV1ovnhzHE2E/',
+    description: '主要负责剪辑和声音设计',
+    resolution: '4k',
+    duration: '02:10',
+    ownerId: 'admin',
+    createdAt: new Date(Date.now() - 86400000).toISOString() // 昨天的日期
+  },
+  {
+    id: '5',
+    title: '沙漠星星酒店',
+    category: 'TVC广告',
+    coverUrl: '',
+    videoUrl: 'https://www.bilibili.com/video/BV1WdqFBvE7g/',
+    description: '主要负责剪辑和声音设计',
+    resolution: '4k',
+    duration: '03:26',
+    ownerId: 'admin',
+    createdAt: new Date(Date.now() - 86400000).toISOString() // 昨天的日期
+  },
+  {
+    id: '6',
+    title: '深理工校招片',
+    category: 'TVC广告',
+    coverUrl: '',
+    videoUrl: 'https://www.bilibili.com/video/BV1ovnhzHEkf/',
+    description: '主要负责剪辑和声音设计',
+    resolution: '4k',
+    duration: '03:05',
+    ownerId: 'admin',
+    createdAt: new Date(Date.now() - 86400000).toISOString() // 昨天的日期
+  },
+  {
+    id: '7',
+    title: '深圳公共建筑预热片',
+    category: '地产广告',
+    coverUrl: '',
+    videoUrl: 'https://www.bilibili.com/video/BV18onGzsEEH/',
+    description: '此影片项目时间特别紧急,在强压力下和团队一起保证影片按时上线,主要负责剪辑和声音设计',
+    resolution: '4k',
+    duration: '02:22',
+    ownerId: 'admin',
+    createdAt: new Date(Date.now() - 86400000).toISOString() // 昨天的日期
+  },
+  {
+    id: '8',
+    title: '越秀和樾府',
+    category: '地产广告',
+    coverUrl: '',
+    videoUrl: 'https://www.bilibili.com/video/BV1BYnhz9E8C/',
+    description: '主要负责剪辑和声音设计',
+    resolution: '4k',
+    duration: '01:49',
+    ownerId: 'admin',
+    createdAt: new Date(Date.now() - 86400000).toISOString() // 昨天的日期
+  },
+  {
+    id: '9',
+    title: '湾流大厦',
+    category: '地产广告',
+    coverUrl: '',
+    videoUrl: 'https://www.bilibili.com/video/BV18onGzsEpS/',
+    description: '主要负责剪辑和声音设计,参与脚本创意设计',
+    resolution: '4k',
+    duration: '03:42',
     ownerId: 'admin',
     createdAt: new Date(Date.now() - 86400000).toISOString() // 昨天的日期
   }
